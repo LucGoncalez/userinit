57f8299b83fadfde9056c617d734db927e443f91
Luciano Gonçalez <dev.lucianogoncalez@gmail.com>
2022-03-10 21:59:22 -0300

    Adicionando testes especiais

--- src/init.sh
+++ src/init.sh
@@ -10,6 +10,12 @@
 # Obs. o diretório <init.d> não deve conter scripts para serem executados
 #
 
+# Testando se o script está sendo executado como source do bash
+if [ "$0" != 'bash' ]; then
+  printf "**ERROR** [userinit]: Este script não está rodando como source do bash.\n" >&2
+  exit 1
+fi
+
 # Configurando diretório base do LKScript
 LKS_BASE_DIR="${HOME}/.lks"
 
@@ -17,9 +23,9 @@ if [ ! -d "$LKS_BASE_DIR" ]; then
   printf "**ERROR** [userinit]: O diretório base <%s> não existe.\n" "$LKS_BASE_DIR" >&2
 else
   # Definindo variáveis
+  LKS_BIN_DIR="${LKS_BASE_DIR}/bin"
   LKS_USERINIT_INITDIR="${LKS_BASE_DIR}/init.d"
   LKS_USERINIT_INITFILE="${LKS_BASE_DIR}/init.tmp"
-  LKS_BIN_DIR="${LKS_BASE_DIR}/bin"
 
   if [ ! -d "$LKS_USERINIT_INITDIR" ] || ( [ -e "$LKS_USERINIT_INITFILE" ] && [ ! -f "$LKS_USERINIT_INITFILE" ] ); then
     printf "**ERROR** [userinit]: O script não está configurado corretamente.\n"
@@ -29,7 +35,7 @@ else
 
     # Carrega configurações para o arquivo de inicialização temporário
     for file in $ "$LKS_USERINIT_INITDIR"/*; do
-      if [ -x "$file" ]; then
+      if [ ! -L "$file" ] && [ -x "$file" ]; then
         printf ". %q\n" "$file"
       fi
     done > "$LKS_USERINIT_INITFILE"
